f7b933f562d64d5b65efaf57fe7633c01fcbb675
khojianvar <khojianvarabdumukhtorov@gmail.com>
2023-06-23 00:16:59 +0500

    Task1

--- thread/thread/Program.cs
+++ thread/thread/Program.cs
@@ -6,7 +6,28 @@ namespace thread
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anvar");
+            Console.Write("Enter the number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int m = 1;
+            while (num > 0)
+            {
+                Thread thread1 = new Thread(() => Calculate(num));
+                thread1.Start();
+                thread1.Name = $"{m}";
+                m++;
+                Console.Write("Enter the number: ");
+                num = int.Parse(Console.ReadLine());
+            }
+        }
+
+        static void Calculate(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.Name}: {i}");
+                Thread.Sleep(1000);
+            }
         }
     }
 }
